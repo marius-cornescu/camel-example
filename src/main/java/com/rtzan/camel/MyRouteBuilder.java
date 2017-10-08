203ec2e3f7c48834b61498678df67049759c1de2
marius_cornescu <marius_cornescu@yahoo.com>
2017-10-08 17:32:24 +0300

    Init camel project

--- src/main/java/com/rtzan/camel/MyRouteBuilder.java
+++ src/main/java/com/rtzan/camel/MyRouteBuilder.java
@@ -33,7 +33,7 @@ public class MyRouteBuilder extends RouteBuilder {
     /**
      * A main() so we can easily run these routing rules in our IDE
      */
-    public static void main(String... args) {
+    public static void main(String... args) throws Exception {
         Main.main(args);
     }
 
--- src/test/java/com/rtzan/camel/PriorityGroupingTest.java
+++ src/test/java/com/rtzan/camel/PriorityGroupingTest.java
@@ -3,14 +3,22 @@
  */
 package com.rtzan.camel;
 
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.AllTests;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 /**
  */
+@RunWith(Arquillian.class)
 public class PriorityGroupingTest {
 
     //~ ----------------------------------------------------------------------------------------------------------------
@@ -20,9 +28,23 @@ public class PriorityGroupingTest {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 
-    //~ ----------------------------------------------------------------------------------------------------------------
-    //~ Methods 
-    //~ ----------------------------------------------------------------------------------------------------------------
+    @Deployment
+    public static JavaArchive deployment() {
+        JavaArchive archive =
+        ShrinkWrap.create(JavaArchive.class)
+                // Camel CDI
+                .addPackage(org.apache.camel.cdi.CdiCamelExtension.class.getPackage())
+                // Test classes
+                .addPackages(true, MyRouteBuilder.class.getPackage())
+                // Bean archive deployment descriptor
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+                ;
+
+
+        //archive.addClass(Account.class);
+        //archive.addAsResource("org/jboss/fuse/persistence/jpa/jpa-camel-context.xml", "META-INF/camel-context.xml");
+        return archive;
+    }
 
     @Before
     public void setUp() {
