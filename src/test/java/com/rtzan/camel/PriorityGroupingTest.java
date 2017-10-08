eebac26b8af25ff5e4e441960ff196bcd6977343
marius_cornescu <marius_cornescu@yahoo.com>
2017-10-08 16:41:51 +0300

    Init camel project

---
+++ src/test/java/com/rtzan/camel/PriorityGroupingTest.java
@@ -0,0 +1,36 @@
+/**
+ * Free
+ */
+package com.rtzan.camel;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ */
+public class PriorityGroupingTest {
+
+    //~ ----------------------------------------------------------------------------------------------------------------
+    //~ Instance fields 
+    //~ ----------------------------------------------------------------------------------------------------------------
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+    //~ ----------------------------------------------------------------------------------------------------------------
+    //~ Methods 
+    //~ ----------------------------------------------------------------------------------------------------------------
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void testPriorityGrouping() throws Exception {
+    }
+
+
+}
